d3943e5c36930c6dd91815330843fee35b94651e
John-Peter Eberhard <johnpetereberhard@gmail.com>
2022-02-27 23:36:41 -0700

    yes

--- src/pages/loginpage/LoginPage.js
+++ src/pages/loginpage/LoginPage.js
@@ -1,7 +1,18 @@
+// import { className } from 'postcss-selector-parser'
 import React from 'react'
+// import Icon from 'react-svg-use'
 // import './styles/global-styles.css'
- import { Link } from 'react-router-dom'
+ import { Link, useNavigate } from 'react-router-dom'
+ import "typeface-dancing-script"
+import { ReactComponent as Logo } from '../../styles/svg/branding/cake-logo.svg'
 export default function LoginPage() {
+    let navigation = useNavigate();
+
+    function goToDash(e){
+        e.preventDefault();
+        console.log("goToDash")
+       navigation('dashboard')
+    }
   return (
     <>
     <nav>
@@ -14,11 +25,47 @@ export default function LoginPage() {
          </li>
        </ul>
      </nav>
-   <header>
-       <h1 className='h1-test'>
-           LOGIN PAGE HERE
-       </h1>
-   </header>
+    
+ 
+   <div className="flex justify-center items-center w-full h-screen">
+	<div className=" w-full h-full"><img className='w-full h-full' src="https://cdn.pixabay.com/photo/2021/05/18/07/56/cakes-6262577_960_720.jpg"></img></div>
+	<form className="item w-full h-full flex flex-col" onClick={goToDash} >
+        <h2 className="login-title">Sam's cake Stand</h2>
+        <Logo className='w-20 h-20'/>
+        <div className='login-form'>
+                    <label className='form-label' for="grid-first-name">
+                    Login
+                    </label>
+                    <input className="form-text" 
+                    id="productTitle" type="text" placeholder="enter username" required/>
+                    
+        </div>
+        <div className='login-form'>
+                    <label className='form-label' for="grid-first-name">
+                    Password
+                    </label>
+                    <input className="form-text" 
+                    id="productTitle" type="password" placeholder="password" required/>
+                    
+        </div> 
+        
+        <button goToDash id="buy-button" type="submit" class="bg-aeroBlue  text-darkPurple hover:text-fieryRose font-bold py-5 px-40 mt-3 rounded-full hover:bg-white ">
+       Login 
+            
+                </button>      
+    </form>
+    </div>
    </>
   )
 }
+
+
+/**
+ *   aeroBlue:'#D7FDF0',
+        cadet: '#556F7A',
+        fieryRose: '#FF5D73',
+        darkPurple: '#4B2142',
+        pinkLavender: '#D4AFCD',
+        isabelline: '#EEE6E6',
+        reallyRed: '#FF0000'
+ */
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      extend:{
+        fontFamily: {
+          'dancingScript': ['Dancing Script', 'cursive']
+        }
+      },
+      colors:{
+        aeroBlue:'#D7FDF0',
+        cadet: '#556F7A',
+        fieryRose: '#FF5D73',
+        darkPurple: '#4B2142',
+        pinkLavender: '#D4AFCD',
+        isabelline: '#EEE6E6',
+        reallyRed: '#FF0000'
+      },
+      container:{
+        center: true,
+        padding: '2rem'
+      },
+    },
   },
   plugins: [],
 }
