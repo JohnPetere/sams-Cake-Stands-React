20e65a5a462e512cbdecb174e04cbc2203594fea
John-Peter Eberhard <johnpetereberhard@gmail.com>
2022-04-08 14:35:28 -0600

    Update ProductEditor.js

--- src/components/ProductEditor/ProductEditor.js
+++ src/components/ProductEditor/ProductEditor.js
@@ -1,3 +1,4 @@
+import ShopEditorEntryForm from 'components/shopEditorEntryForm/ShopEditorEntryForm';
 import React from 'react';
 
 import Styles from './styles.module.css'
@@ -5,9 +6,8 @@ import Styles from './styles.module.css'
 function ProductEditor ({children, ...props})  {
   return (
         <div className={Styles.producteditor}>
-           <h2>ProductEditor Component</h2>
-           {children}
-
+           
+           <ShopEditorEntryForm/>
         </div>
   )
 }
