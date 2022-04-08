5d01eb086a4f8044065795e4d43017d809db0afa
John-Peter Eberhard <johnpetereberhard@gmail.com>
2022-04-08 12:47:14 -0600

    Update ShopEditor.js

--- src/components/shopEditor/ShopEditor.js
+++ src/components/shopEditor/ShopEditor.js
@@ -2,9 +2,6 @@ import ShopCardLoader from 'components/shopCardLoader/ShopCardLoader';
 import ShopEditorEntryForm from 'components/shopEditorEntryForm/ShopEditorEntryForm';
 import React from 'react';
 
-// import ShopEditorEntryForm from 'components/shopEditorEntryForm/ShopEditorEntryForm';
-// import ShopCardLoader from 'components/shopCardLoader/ShopCardLoader';
-
 function ShopEditor ({children,productName,handleProductName, ...props})  {
     // console.log(productName)
     // console.log("handleProductName", handleProductName)
