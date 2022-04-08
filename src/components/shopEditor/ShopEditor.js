e86324c984e93bd63bafb7227d847d2b187fd30d
John-Peter Eberhard <johnpetereberhard@gmail.com>
2022-04-08 12:43:21 -0600

    sss

--- src/components/shopEditor/ShopEditor.js
+++ src/components/shopEditor/ShopEditor.js
@@ -1,7 +1,9 @@
+import ShopCardLoader from 'components/shopCardLoader/ShopCardLoader';
+import ShopEditorEntryForm from 'components/shopEditorEntryForm/ShopEditorEntryForm';
 import React from 'react';
 
-import ShopEditorEntryForm from 'components/shopEditorEntryForm/ShopEditorEntryForm';
-import ShopCardLoader from 'components/shopCardLoader/ShopCardLoader';
+// import ShopEditorEntryForm from 'components/shopEditorEntryForm/ShopEditorEntryForm';
+// import ShopCardLoader from 'components/shopCardLoader/ShopCardLoader';
 
 function ShopEditor ({children,productName,handleProductName, ...props})  {
     // console.log(productName)
