9964dac232a753d4168598a677e62c5a083c4c88
John-Peter Eberhard <johnpetereberhard@gmail.com>
2022-04-08 05:04:35 -0600

    Revert "s"
    This reverts commit 3a459e59729be90297b92802dcfa0db63ea36207.

--- src/components/shopEditor/ShopEditor.js
+++ src/components/shopEditor/ShopEditor.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import './styles.module.css'
+import Styles from './styles.module.css'
 import ShopEditorEntryForm from 'components/shopEditorEntryForm/ShopEditor';
 import ShopCardLoader from 'components/shopCardLoader/ShopCardLoader';
-
+// import shopPreview from 'components/shopCardLoader/ShopCardLoader';
 function ShopEditor ({children,productName,handleProductName, ...props})  {
     console.log(productName)
     console.log("handleProductName", handleProductName)
