12239beac4796680b4c8420fc96a42555901ac77
John-Peter Eberhard <johnpetereberhard@gmail.com>
2022-04-08 14:42:02 -0600

    Added styling to ProductEditor

--- src/components/ProductEditor/ProductEditor.js
+++ src/components/ProductEditor/ProductEditor.js
@@ -2,11 +2,11 @@ import ShopCardLoader from 'components/shopCardLoader/ShopCardLoader';
 import ShopEditorEntryForm from 'components/shopEditorEntryForm/ShopEditorEntryForm';
 import React from 'react';
 
-import Styles from './styles.module.css'
+// import Styles from './styles.module.css'
 
 function ProductEditor ({children, ...props})  {
   return (
-        <div className={Styles.producteditor}>
+        <div id="shop-item" className="flex">
            
            <ShopEditorEntryForm/>
           <ShopCardLoader/>
