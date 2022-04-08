381313ae2fe8741a80c05f1b9f1994a1d2ac0f79
John-Peter Eberhard <johnpetereberhard@gmail.com>
2022-04-08 05:09:43 -0600

    Clean ujp

--- src/components/shopEditor/ShopEditor.js
+++ src/components/shopEditor/ShopEditor.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Styles from './styles.module.css'
-import ShopEditorEntryForm from 'components/shopEditorEntryForm/ShopEditor';
+import ShopEditorEntryForm from 'components/shopEditorEntryForm/ShopEditorEntryForm';
 import ShopCardLoader from 'components/shopCardLoader/ShopCardLoader';
-// import shopPreview from 'components/shopCardLoader/ShopCardLoader';
+import './styles.module.css'
+
 function ShopEditor ({children,productName,handleProductName, ...props})  {
     console.log(productName)
     console.log("handleProductName", handleProductName)
